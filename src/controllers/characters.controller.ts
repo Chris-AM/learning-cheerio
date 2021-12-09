9320e4e50945ffc8cd18f7e6e318907eb9e54213
chris.am <c.aranguizm@outlook.com>
2021-12-09 12:31:42 -0300

    characters controller and route updated following TypeORM documentation

--- src/controllers/characters.controller.ts
+++ src/controllers/characters.controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { CharacterEntity } from "../entity/characterEntity";
 
-const getCharacters = (req: Request, res: Response) => {
+const getCharacters = async (req: Request, res: Response): Promise<Response> => {
   try {
-    res.json({
+    const characters = await getRepository(CharacterEntity).find();
+    return res.json({
       message: "getCharacters",
+      characters,
     });
   } catch (error) {
     res.status(500).json({
@@ -11,23 +15,97 @@ const getCharacters = (req: Request, res: Response) => {
       message: "Internal server error. Check the logs.",
     });
     console.log(error);
+    throw (error);
   }
 };
 
-const getCharacterById = (req: Request, res: Response) => {
-  const id = req.params.id;
-    try {
-        res.json({
-            message: "getCharacterById",
-            id
-        });
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            message: "Internal server error. Check the logs.",
-        });
-        console.log(error);
-    }
+const getCharacterById = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+    const character = await getRepository(CharacterEntity).findOne(id);
+    return res.json({
+      message: "getCharacter",
+      character,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: "Internal server error. Check the logs.",
+    });
+    console.log(error);
+    throw (error);
+  }
 };
 
-export { getCharacters, getCharacterById };
+const createCharacter = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const newCharacter = await getRepository(CharacterEntity).create(req.body);
+    const createdCharacter = await getRepository(CharacterEntity).save(newCharacter);
+    return res.json({
+      message: "createCharacter",
+      createdCharacter,
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: "Internal server error. Check the logs.",
+    });
+    console.log(error);
+    throw (error);
+  }
+};
+
+const updateCharacter = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+    const character = await getRepository(CharacterEntity).findOne(id);
+    if (!character) {
+      return res.status(404).json({
+        ok: false,
+        message: "Character not found.",
+      });
+    }else{
+      getRepository(CharacterEntity).merge(character, req.body);
+      const updatedCharacter = await getRepository(CharacterEntity).save(character);
+      return res.json({
+        message: "updateCharacter",
+        updatedCharacter,
+      });
+    }
+} catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: "Internal server error. Check the logs.",
+    });
+    console.log(error);
+    throw (error);
+  }
+};
+
+const deleteCharacter = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const { id } = req.params;
+    const character = await getRepository(CharacterEntity).findOne(id);
+    if (!character) {
+      return res.status(404).json({
+        ok: false,
+        message: "Character not found.",
+      });
+    }else{
+      await getRepository(CharacterEntity).delete(id);
+      return res.json({
+        message: "deleteCharacter",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: "Internal server error. Check the logs.",
+    });
+    console.log(error);
+    throw (error);
+  }
+};
+
+export { getCharacters, getCharacterById, createCharacter, updateCharacter, deleteCharacter };
--- src/routes/characters.routes.ts
+++ src/routes/characters.routes.ts
@@ -1,13 +1,20 @@
 import {
   getCharacters,
   getCharacterById,
+  createCharacter,
+  updateCharacter,
+  deleteCharacter,
 } from "../controllers/characters.controller";
 
 import { Router } from "express";
 
 const charactersRouter = Router();
 
-charactersRouter.get("/", getCharacters);
-charactersRouter.get("/:id", getCharacterById);
+charactersRouter.get("/characters", getCharacters);
+charactersRouter.get("/characters/:id", getCharacterById);
+charactersRouter.post("/characters", createCharacter);
+charactersRouter.put("/characters/:id", updateCharacter);
+charactersRouter.delete("/characters/:id", deleteCharacter);
+
 
 export default charactersRouter;
